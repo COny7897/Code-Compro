fe5be679a770cedbe9bac3c6d7a072cc4e5b69d2
COny_Knight <97920186+COny7897@users.noreply.github.com>
2023-07-13 11:25:58 +0700

    Exercise - Chapter 2

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+	printf("%-40s%40s\n","Programming","Computer"); 
+	return 0;
+}
---
+++ 2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+	printf("%-40s%40s%1920s","Computer","Programming","I am a hard-working student."); 
+	return 0;
+}
